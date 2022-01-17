926e50ec7a9780bffca7475bc24c4b9c551d3c22
Kenneth Kebaara <kkebaara@yahoo.com>
2022-01-17 04:57:08 -0700

    commit tree fa066e6cf9ef9da5b9087d4734b2f34b3dfad75e

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(arrDrivers, string){
+    return arrDrivers.filter(function(name){return name.toLowerCase() === string.toLowerCase();});
+  }
+
+function fuzzyMatch(arrDrivers, string){
+    return arrDrivers.filter(function(name){return name.toLowerCase()[0] === string.toLowerCase()[0];});
+    }
+
+function matchName(arrDrivers, string){
+    return arrDrivers.filter(function(key){return key.name === string})
+}
